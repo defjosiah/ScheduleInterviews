25b203a7d7b3dbfb4a6e753eb2df01c3d598cdc2
Josiah Grace <josiahbgrace@gmail.com>
2014-02-12 04:20:26 -0600

    correct csv parse

--- schedule.py
+++ schedule.py
@@ -1,3 +1,5 @@
+import csv
+
 #Josiah Grace
 #O-Week 2014 Scheduling
 
@@ -23,13 +25,47 @@ Interview Scheduler
 # (date) : [(start, end),(start, end)]
 # TODO: remove hard-coded form of coord_avail
 coord_avail = { 
-        ("Fri, 14th") : [(15.0, 18.0)],
-        ("Sat, 15th") : [(10.0, 19.0)],
-        ("Sun, 16th") : [(10.0, 14,0), (19.0, 21.0)],
-        ("Fri, 21st") : [(15.0, 20.0)],
-        ("Sat, 22nd") : [(10.0, 19.0)],
-        ("Sun, 23rd") : [(10.0, 14,0), (19.0, 21.0)]
+        ("Fri 14th") : [(15.0, 18.0)],
+        ("Sat 15th") : [(10.0, 19.0)],
+        ("Sun 16th") : [(10.0, 14,0), (19.0, 21.0)],
+        ("Fri 21st") : [(15.0, 20.0)],
+        ("Sat 22nd") : [(10.0, 19.0)],
+        ("Sun 23rd") : [(10.0, 14,0), (19.0, 21.0)]
     }
+
+def parse_csv(filename):
+    """
+    Input: Excel CSV file
+    Parse input into list of available times
+    Return: dictionary mapping
+    (name) : {(date) : excluded_times (start, end)}
+
+    CSV Format (Google spreadsheet): 
+    Name date1                     date2        date3
+    a    start1 end1 start2 end2   
+    b
+    c 
+    """
+    name_exlude = {}
+    dates = []
+    with open(filename, 'rb') as csvfile:
+        schedule_reader = csv.reader(csvfile, delimiter=',')
+        for i, row in enumerate(schedule_reader):
+            if(i == 0):
+                dates = row[1::]
+            else:
+                exclude = {}
+                for j, date in enumerate(dates):
+                    no_work = row[j+1].split()
+                    temp = []
+                    for k in range(0, len(no_work), 2):
+                        temp.append((float(no_work[k]), float(no_work[k+1])))
+                    exclude[date] = temp
+                name_exlude[row[0]] = exclude
+    return name_exlude
+
+
+
 
 def coordinator_availability(pattern):
     """
@@ -57,5 +93,9 @@ def coordinator_availability(pattern):
         block_avail[date] = times
     return block_avail
 
+
+
 def compare_close(a, b, error):
     return abs(a-b) < error
+
+print parse_csv("test_schedule_1.csv")
