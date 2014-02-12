3ec7cb8a678c685b98f6101f1e79b89bd234bb67
Josiah Grace <josiahbgrace@gmail.com>
2014-02-12 03:26:09 -0600

    added working block scheduler

--- schedule.py
+++ schedule.py
@@ -16,6 +16,43 @@ Interview Scheduler
     times
 
     (Optional)
-    	Turn back into Google Doc spreadsheet
+        Turn back into Google Doc spreadsheet
 
 """
+
+# (date) : [(start, end),(start, end)]
+# TODO: remove hard-coded form of coord_avail
+coord_avail = { 
+        ("Fri, 14th") : [(15.0, 18.0)],
+        ("Sat, 15th") : [(10.0, 19.0)],
+        ("Sun, 16th") : [(10.0, 14,0), (19.0, 21.0)],
+        ("Fri, 21st") : [(15.0, 20.0)],
+        ("Sat, 22nd") : [(10.0, 19.0)],
+        ("Sun, 23rd") : [(10.0, 14,0), (19.0, 21.0)]
+    }
+
+def coordinator_availability(pattern):
+    """
+    Input: pattern is a tuple of (interview length, break)
+    Using the coordinator availability dictionary above, and 
+    a given pattern, split the interview availability into 
+    pattern specific block, output is dictionary of form 
+    --(date) : [available blocks]--
+    """
+
+    int_length = pattern[0]
+    break_length = pattern[1]
+    block_avail = {}
+
+    for date in coord_avail:
+        times = []
+        for strt_end in coord_avail[date]:
+            start = strt_end[0]*60
+            end = strt_end[1]*60
+            while(start < end):
+                times.append(start/60)
+                start += int_length + break_length
+        block_avail[date] = times
+    return block_avail
+
+print coordinator_availability((15, 5))
