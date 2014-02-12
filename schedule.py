c72deb6f02330a5e0f380b922935475c6fa58783
Josiah Grace <josiahbgrace@gmail.com>
2014-02-12 02:12:44 -0600

    schedule.py initialization

---
+++ schedule.py
@@ -0,0 +1,18 @@
+#Josiah Grace
+#O-Week 2014 Scheduling
+
+
+"""
+Interview Scheduler
+
+    (Optional) 
+        Access the Google Docs API in order to download the CSV
+        files from the duncaroos@gmail folder.
+
+    a. Transform from CSV into acceptable interview format
+    b. Accept different interview blocks for coordinators 
+    (e.g. 15 minutes w/ 5 minute breaks)
+    c. Turn candidate preferences into acceptable interview
+    times
+
+"""
