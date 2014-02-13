68812df58682fc7f309bb6163458851f0ff9762e
Josiah Grace <josiahbgrace@gmail.com>
2014-02-12 21:08:28 -0600

    final commenting, cleaning up code

--- schedule.py
+++ schedule.py
@@ -35,16 +35,17 @@ coord_avail = {
 
 def parse_csv(filename):
     """
-    Input: Excel CSV file
+    Input: .csv filename string
     Parse input into list of available times
     Return: dictionary mapping
-    (name) : {(date) : excluded_times (start, end)}
-
+        (name) : {(date) : excluded_times (start, end)}
+    ______________________________________________________
     CSV Format (Google spreadsheet): 
-    Name date1                     date2        date3
-    a    start1 end1 start2 end2   
-    b
-    c 
+        Name date1                     date2        date3
+        a    start1 end1 start2 end2   ...          ...
+        b
+        c 
+    ______________________________________________________
     """
     name_exclude = {}
     dates = []
@@ -64,21 +65,20 @@ def parse_csv(filename):
                 name_exclude[row[0]] = exclude
     return name_exclude
 
-
-
-
 def coordinator_availability(pattern):
     """
-    Input: pattern is a tuple of (interview length, break)
-    in minutes
-    Using the coordinator availability dictionary above, and 
-    a given pattern, split the interview availability into 
-    pattern specific block, output is dictionary of form 
-    --(date) : [available blocks]-- blocks are start times in 
-    (hour.decimal_minutes) format
+    Input: 
+        pattern - tuple of (interview length, break) in min
+        coord_avail - global dict mapping dates to availability (hours)
+        (date) : [(start, end), (start,end)]
+    
+    Returns: block_avail - split the interview availability into 
+    pattern specific block, output is dictionary of form:
+    (date) : [start1, start2, start3]
+    where each list entry is the start time of an interview in 
+    (hour.decimal_minute) format
     """
-
-    int_length = pattern[0]
+    interview_length = pattern[0]   #interview length
     break_length = pattern[1]
     block_avail = {}
 
@@ -89,27 +89,23 @@ def coordinator_availability(pattern):
             end = strt_end[1]*60
             while(start < end):
                 times.append(start/60)
-                start += int_length + break_length
+                start += interview_length + break_length
         block_avail[date] = times
     return block_avail
 
-
-
-def compare_close(a, b, error):
-    return abs(a-b) < error
-
-def is_between(start_end, time):
-    return time >= start_end[0] and time <= start_end[1] 
-
-def is_in(available, excluded_times):
-    test = []
-    for time in excluded_times:
-        test.append( is_between(time, available) )
-    return any(test)
-
 def available_times(name_exclude, block_avail):
     """
-    return: (name) : {(dates) : times}
+    Input:
+        name_exclude - dictionary consisting of
+            (name) : {(date) : excluded_times (start, end)}
+        block_avail - dictionary consisting of 
+                (date) : [start1, start2, start3]
+    Returns: 
+        name_available - (name) : {(dates) : times}
+
+    Reduces excluded times and available startimes interviews 
+    into a single dictionary mapping names to available interview
+    start times per input date. 
     """ 
     name_available = {}
     for person in name_exclude:
@@ -124,8 +120,22 @@ def available_times(name_exclude, block_avail):
 
 def sort_and_match(name_available, block_avail):
     """
-    Match by available_times
-    Return dict: (Date) : {time: person}
+    Input: 
+        name_available - (name) : {(dates) : times}
+        mapping names to available interview start times
+        block_avail - (date) : [start1, start2, start3]
+        mapping dates to all possible interview start times
+
+    Return: 
+        matching_dict - (date) : {(time): name}
+        not_matched - [name1, name2, name3]
+
+    Reduces availabe interview start times per person to actual 
+    interview mappings per person by attempting to match the most
+    difficult inputs (smallest number of available interview times)
+    to interview times by least to most popular. 
+    ** variation on longest task first scheduling - Decreasing Time **
+
     """
     most_popular = get_most_popular(name_available, block_avail)
 
@@ -147,62 +157,18 @@ def sort_and_match(name_available, block_avail):
     for person in sorted(name_sort, key=lambda x:x[1]):
         found = False
         for time_date in sorted(most_popular, key=lambda x: x[2]):
-            if(close_enough_in_date(time_date[1], name_available[person[0]][time_date[0]])):
+            if(close_enough_in_date(time_date[1], 
+                name_available[person[0]][time_date[0]])):
+
                 matching_dict[time_date[0]][time_date[1]] = person[0]
                 most_popular.remove(time_date)
                 found = True
                 break
+
         if found == False:
             not_matched.append(person[0])
 
-    pretty_print_interviews(matching_dict)
     return matching_dict, not_matched
-
-def close_enough_in_date(time, time_list):
-    for x in time_list:
-        if(compare_close(time, x, 0.001)):
-            return True
-    return False
-
-def get_most_popular(name_available, block_avail):
-
-    popular = {}
-    for date in block_avail:
-        popular[date] = {}
-    for date in popular:
-        for entry in block_avail[date]:
-            popular[date][entry] = 0 
-
-    for name in name_available:
-        for date in name_available[name]:
-            for entry in name_available[name][date]:
-                popular[date][entry] += 1
-    
-    return_list = []
-    for date in popular:
-        for entry in popular[date]:
-            return_list.append( (date, entry, popular[date][entry]) )
-
-    return return_list
-
-def pretty_print_interviews(matching_dict):
-    print "Interview Times"
-    for date in sorted(matching_dict, key=lambda x: x):
-        print date + ":"
-        for time in sorted(matching_dict[date], key=lambda x: x):
-            print "\t {0} -- {1}".format(float_mil_to_actual(time), 
-                                            matching_dict[date][time])
-
-def float_mil_to_actual(time):
-    if(time > 12.0):
-        final_time = int(time - 12.0)
-    else:
-        final_time = int(time)
-    decimal = time % 1
-    actual = int(round(decimal * 60))
-    if(actual == 0):
-        actual = "00"
-    return "{0}:{1}".format(final_time, actual)
 
 def write_to_csv(outfile, matching_dict, block_avail):
     csv_out = []
@@ -232,7 +198,111 @@ def write_to_csv(outfile, matching_dict, block_avail):
     with open(outfile, 'w') as fp:
         a = csv.writer(fp, delimiter=',')
         a.writerows(csv_out)
+    return
 
+#########################################################
+############# Helper Functions ##########################
+#########################################################
+
+def get_most_popular(name_available, block_avail):
+    """
+    Input: 
+        name_available - (name) : {(dates) : times}
+        mapping names to available interview start times
+        block_avail - (date) : [start1, start2, start3]
+        mapping dates to all possible interview start times
+
+    Return: 
+        List of tuples with (date, time, popularity count)
+
+    Reduce name_available and block_avail into a list of 
+    tuples with counts for how often each time occured in 
+    input availabilities. Used for sorting by popularity 
+    in sort and match.
+    """
+    popular = {}
+    for date in block_avail:
+        popular[date] = {}
+    for date in popular:
+        for entry in block_avail[date]:
+            popular[date][entry] = 0 
+
+    for name in name_available:
+        for date in name_available[name]:
+            for entry in name_available[name][date]:
+                popular[date][entry] += 1
+    
+    return_list = []
+    for date in popular:
+        for entry in popular[date]:
+            return_list.append( (date, entry, popular[date][entry]) )
+
+    return return_list
+
+def pretty_print_interviews(matching_dict):
+    """
+    Print in form: 
+    ______________________________________________________
+        Interview Times
+            (Date):
+                 (time1) -- (name1)
+                 (time2) -- (name2)
+    ______________________________________________________
+    """
+    print "Interview Times"
+    for date in sorted(matching_dict, key=lambda x: x):
+        print date + ":"
+        for time in sorted(matching_dict[date], key=lambda x: x):
+            print "\t {0} -- {1}".format(float_mil_to_actual(time), 
+                                            matching_dict[date][time])
+
+def float_mil_to_actual(time):
+    """
+    Change float format (date.decimal_minute)
+    to acceptable time string. Rounded to nearest
+    standard time.
+    e.g. 10.33 -> "10:20"
+    """
+    if(time > 12.0):
+        final_time = int(time - 12.0)
+    else:
+        final_time = int(time)
+    decimal = time % 1
+    actual = int(round(decimal * 60))
+    if(actual == 0):
+        actual = "00"
+    return "{0}:{1}".format(final_time, actual)
+
+def close_enough_in_date(time, time_list):
+    """
+    Check if an input time is in a list of 
+    times within 0.01 error, prevents float comparison
+    issues. 
+    """
+    for x in time_list:
+        if(compare_close(time, x, 0.001)):
+            return True
+    return False
+
+def compare_close(a, b, error):
+    return abs(a-b) < error
+
+def is_between(start_end, time):
+    return time >= start_end[0] and time <= start_end[1] 
+
+def is_in(available, excluded_times):
+    """
+    Check of an input time is in a list of (start, end)
+    excluded times. 
+    """
+    test = []
+    for time in excluded_times:
+        test.append( is_between(time, available) )
+    return any(test)
+
+#########################################################
+############# End Helper Functions ######################
+#########################################################
 
 def main():
     name_exclude = parse_csv("test_schedule_1.csv")
